b69704db1fba3c907ab43a4565d0de2d547d85ad
DevenMahesh <pesug20cs107@pesu.pes.edu>
2023-12-07 16:35:00 +0530

    init14

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -18,8 +18,11 @@ import Aws from '../assets/aws.png';
 const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#CDF5FD] text-black-300 md:text-2xl'>
+
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+      
+
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-blue-700 '>Skills</p>
               <p className='py-4'> These are the technologies I've worked with</p>
